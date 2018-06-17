a1c27bf5e1ffaec5ea58322e8296481664fdf721
Misael Calvillo Mancilla <calvillomisa@hotmail.com>
2018-06-17 03:14:45 -0700

    Setup: Add React Router to the project
        * Install the npm package 'react-router-dom'

--- src/index.js
+++ src/index.js
@@ -4,11 +4,18 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+// Import BrowserRouter
+import { BrowserRouter } from 'react-router-dom';
+
 // Import Bootstrap styles
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 // Import the global styles
 import './assets/css/styles.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+, document.getElementById('root'));
 registerServiceWorker();
