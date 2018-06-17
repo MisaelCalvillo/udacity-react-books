86b357282100e9d9cba620dba46b2f6ab0dafc07
Misael Calvillo Mancilla <calvillomisa@hotmail.com>
2018-06-17 02:17:14 -0700

    Setup: Add SCSS capability to the project and docs
    The SCSS files should be inserted inside './assets/scss' folder in
    order to be processed as CSS in the folder './assets/css'
    
        * The SCSS functionality to the app is added with the npm package
          'node-sass-chokidar'
    
        * The package 'npm-run-all' gives node the ability to run 2 commands
          at the same time

---
+++ docs/instructions.md
@@ -0,0 +1,5 @@
+#### To start the app you need to run this command in the terminal:
+
+ `npm start`
+
+> This command will start the server to see the app and will compile all the SCSS code into CSS.
--- src/index.js
+++ src/index.js
@@ -4,5 +4,8 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+// Import the global styles
+import './assets/css/styles.css';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
